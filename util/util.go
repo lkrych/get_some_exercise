d926a10f87624a3b5dcd91d541534c51d70840df
Leland Krych <Leland.Krych@gmail.com>
2019-03-05 22:30:23 -0800

    export some util functions and add getFileSuffix to util

--- util/util.go
+++ util/util.go
@@ -5,6 +5,21 @@ import (
 	"log"
 )
 
+func GetFileSuffix(language string) string {
+	switch language {
+	case "python":
+		return ".py"
+	case "go":
+		return ".go"
+	case "c":
+		return ".c"
+	case "elixir":
+		return ".ex"
+	}
+	//default to go
+	return ".go"
+}
+
 // print funcs for formatting
 func printSpace() {
 	fmt.Printf("\n")
@@ -14,7 +29,7 @@ func printBorder() {
 	fmt.Printf("==================================================================== \n")
 }
 
-func formatPrint(printThis string) {
+func FormatPrint(printThis string) {
 	printBorder()
 	fmt.Println(printThis)
 	printBorder()
@@ -22,7 +37,7 @@ func formatPrint(printThis string) {
 }
 
 //
-func checkErr(e error) {
+func CheckErr(e error) {
 	if e != nil {
 		log.Fatal(e)
 		panic(e)
