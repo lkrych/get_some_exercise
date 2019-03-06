6ab000a809c4aa36b3cf7ba865c617d57de7dd4a
Leland Krych <Leland.Krych@gmail.com>
2019-03-05 22:31:21 -0800

    test exercises

---
+++ do_some_exercises/exercises.go
@@ -0,0 +1,7 @@
+package goExercises
+
+//implement a data structure stackOfStacks (setOfStacks really) that is composed of
+//several stacks and should create a new stack once the previous one exceeds capacity
+//push and pop should behave identically to a single stack
+type stackOfStacks struct{}
+ 
