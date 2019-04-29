4fc5c6f6e1f11b0d29b88e2d17de1f5a2a03db83
Leland Krych <Leland.Krych@gmail.com>
2019-04-29 07:55:57 -0700

    run ocaml exercise in generator

--- do_some_exercises/exercises.ml
+++ do_some_exercises/exercises.ml
@@ -1,6 +1,5 @@
 (* last returns the last element of a list *)
-
 let rec last = function
-  | [] -> None
-  | x :: [] -> Some x
-  | x :: tail -> last tail
+| [] -> None
+| a::[] -> Some a
+| h::t -> last t
