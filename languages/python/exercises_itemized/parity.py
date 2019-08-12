14cd4d914fea9ac4b28ab198364fb828da3abb7e
Leland Krych <Leland.Krych@gmail.com>
2019-08-12 09:02:34 -0700

    Add parity

---
+++ languages/python/exercises_itemized/parity.py
@@ -0,0 +1,3 @@
+# The parity of a binary value is 1 if the number of 1s in the word is odd 
+def parity(x:int) -> int:
+    return 0
---
+++ languages/python/exercises_test/parity_test.py
@@ -0,0 +1,27 @@
+class TestParity(unittest.TestCase):
+    def test_5(self):
+        """
+        Test that parity can handle small integers
+        """
+        result = parity(5)
+        self.assertEqual(result, 0)
+
+    def test_6(self):
+        """
+        Test that parity can handle small integers
+        """
+        result = parity(6)
+        self.assertEqual(result, 0)
+
+    def test_8(self):
+        """
+        Test that parity can handle small integers
+        """
+        result = parity(8)
+        self.assertEqual(result, 1)
+    def test_65536(self):
+        """
+        Test that parity can handle larger integers
+        """
+        result = parity(65536)
+        self.assertEqual(result, 1)
