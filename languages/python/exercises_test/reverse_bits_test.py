5a412ba2595e79a6d9c1c4b78a2b2ce5c5a53ebe
Leland Krych <Leland.Krych@gmail.com>
2019-08-12 16:51:47 -0700

    add tests for reverse_bits

---
+++ languages/python/exercises_test/reverse_bits_test.py
@@ -0,0 +1,27 @@
+class TestReverseBits(unittest.TestCase):
+    def test_5(self):
+        """
+        Test that reverse_bits can handle small integers
+        """
+        result = reverse_bits(5)
+        self.assertEqual(result, 1)
+
+    def test_6(self):
+        """
+        Test that reverse_bits can handle small integers
+        """
+        result = reverse_bits(6)
+        self.assertEqual(result, 3)
+
+    def test_243(self):
+        """
+        Test that reverse_bits can handle small integers
+        """
+        result = reverse_bits(243)
+        self.assertEqual(result, 207)
+    def test_65536(self):
+        """
+        Test that reverse_bits can handle larger integers
+        """
+        result = reverse_bits(65536)
+        self.assertEqual(result, 1)
