dc7acfdf765772e3a927186780933ff6ed5bd8d5
Leland Krych <Leland.Krych@gmail.com>
2019-08-13 09:18:22 -0700

    Add the exercise

---
+++ languages/python/exercises_itemized/same_weight.py
@@ -0,0 +1,5 @@
+# the weight of a non-negative integer is the number of bits set to 1 in its binary representation
+# write a program that takes as input a non-negative integer and returns a number y
+# which is not equal to x, but has the same weight and their difference is as small as possible
+def same_weight(x: int) -> int:
+    return 0
