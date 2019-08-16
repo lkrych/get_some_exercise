867ecf1c8d07ab998bf5a88854a84a306da40e31
Leland Krych <Leland.Krych@gmail.com>
2019-08-15 20:03:02 -0700

    create exercise, soln and test for multiplication with bitwise

---
+++ languages/python/exercises_itemized/mult_with_bitwise.py
@@ -0,0 +1,4 @@
+# write a program that multiples two nonnegative integers, the only operators you are allowed
+# to use are assignment, bitwise operators, equality checks and Boolean combinations
+def multiply(x: int, y: int) -> int:
+    return 0
