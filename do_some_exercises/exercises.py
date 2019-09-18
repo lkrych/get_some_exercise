bfef009d4c32abf27899288841a04d564783d742
Leland Krych <Leland.Krych@gmail.com>
2019-09-18 09:22:55 -0700

    do simple exercise

--- do_some_exercises/exercises.py
+++ do_some_exercises/exercises.py
@@ -1,8 +1,8 @@
 #given two positive integers, compute their quotient using only the addition, subtraction and shifting operators
 def divide(x: int, y: int) -> int:
-    q = 0
-    while x >= y:
+    quotient = 1
+    while x > y:
         x -= y
-        q += 1
-    return q
+        quotient += 1
+    return quotient
  
