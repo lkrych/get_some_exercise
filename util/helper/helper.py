0e137b244de567568f1a683b92b99b6373c57ee5
Leland Krych <Leland.Krych@gmail.com>
2019-10-10 15:18:01 -0700

    mock helper for linked list construction

--- util/helper/helper.py
+++ util/helper/helper.py
@@ -5,14 +5,17 @@ class Node():
 
 class LinkedList():
     def __init__(self, data):
-        self.root = self.create_ll(data)
+        if type data is List: #stub out until you know proper syntax
+            self.root = self.create_ll_arr(data)
+        else
+            self.root = self.create_ll_node(data)
+        self.data = data
     
-    def create_ll(self, arr):
+    def create_ll_from_arr(self, arr):
         root = Node(arr[0])
         current_node = root
-        for i in range(arr[1:]):
-            new_node = Node(arr[i])
+        for _ , val in enumerate(arr[1:]):
+            new_node = Node(val)
             current_node.next = new_node
             new_node = current_node
-
         return root
