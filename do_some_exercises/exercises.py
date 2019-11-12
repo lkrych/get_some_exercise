aa695c666ed4f7dfebc4ab5cd61f1b78e741bdb1
Leland Krych <Leland.Krych@gmail.com>
2019-11-11 21:39:46 -0800

    get some exercise

--- do_some_exercises/exercises.py
+++ do_some_exercises/exercises.py
@@ -1,25 +1,11 @@
-from typing import List
+def bubble_sort(arr):
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                sorted = False
+    return arr
 
-def merge_sort(arr: List[int]) -> List[int]:
-    if len(arr) <= 1:
-        return arr
-    mid_idx = len(arr) // 2
-    left_arr = merge_sort(arr[:mid_idx])
-    right_arr = merge_sort(arr[mid_idx:])
-    return merge(left_arr, right_arr)
 
-    
-
-def merge(arr1: List[int], arr2: List[int]) -> List[int]:
-    merged = []
-    i,j = 0,0
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] <= arr2[j]:
-            merged.append(arr1[i])
-            i += 1
-        else:
-            merged.append(arr2[j])
-            j += 1
-    merged.extend(arr1[i:])
-    merged.extend(arr2[j:])
-    return merged
--- do_some_exercises/exercises_solns.py
+++ do_some_exercises/exercises_solns.py
@@ -1,42 +1,20 @@
-# #merge sort
-# def merge_sort(arr):
-#   #base case, the arr is sorted if it is of length 1 or 0
-#   if len(arr) < 2: 
-#     return arr
-  
-#   #divide the array in two
-#   mid_idx = len(arr) // 2
-#   left_arr = arr[: mid_idx]
-#   right_arr = arr[mid_idx :]
+# #bubble sort
+# def bubble_sort(arr):
+#   sorted = False
+#   while not sorted: #start bubbling
+#     sorted = True #sort until there are no more swaps
+#     for i, el in enumerate(arr):
+#       #avoid bounds error with this check
+#       if i < len(arr) - 1:
+#         if el > arr[i+1]:
+#           swap(arr, i, i+1)
+#           sorted = False
+#   return arr
 
-#   #recursively sort the two arrays
-#   sorted_left_arr = merge_sort(left_arr)
-#   sorted_right_arr = merge_sort(right_arr)
-
-#   #use helper function to merge the two sorted arrays
-#   return merge(sorted_left_arr, sorted_right_arr)
-
-# #helper function for merge sort
-# def merge(arr1, arr2):
-#   merged = []
-#   i = 0 #init indices
-#   j = 0
-#   while len(merged) < len(arr1) + len(arr2):
-#     #if either array has been completely checked, add the other array to merged
-#     if i >= len(arr1):
-#       merged.extend(arr2[j:])
-#       return merged
-#     elif j >= len(arr2):
-#       merged.extend(arr1[i:])
-#       return merged
-
-#     #if neither array is empty, check which element is smaller and add to merged
-#     if arr1[i] > arr2[j]:
-#       merged.append(arr2[j])
-#       j+=1
-#     else:
-#       merged.append(arr1[i])
-#       i+=1
-
-#   return merged
+# #helper function for bubble sort
+# def swap(arr, i, j):
+#   temp = arr[i]
+#   arr[i] = arr[j]
+#   arr[j] = temp
+#   return arr
  
