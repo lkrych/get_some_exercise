56cc0ca49824bd64b02f244840f9552a03651e58
Leland Krych <Leland.Krych@gmail.com>
2019-12-06 09:10:50 -0800

    do some exercises

--- do_some_exercises/exercises.py
+++ do_some_exercises/exercises.py
@@ -1,11 +1,11 @@
 def bubble_sort(arr):
     sorted = False
-    while not sorted:
+    while (not sorted):
         sorted = True
         for i in range(len(arr) - 1):
             if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
+                arr[i],arr[i+1] = arr[i+1],arr[i]
                 sorted = False
     return arr
 
-
+ 
