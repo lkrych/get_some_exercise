b4048497b070ce63ea0f101f79f3bff5b60c0e08
Leland Krych <Leland.Krych@gmail.com>
2019-12-08 19:20:35 -0800

    get some exercise

--- do_some_exercises/exercises.py
+++ do_some_exercises/exercises.py
@@ -1,11 +1,14 @@
-def bubble_sort(arr):
-    sorted = False
-    while (not sorted):
-        sorted = True
-        for i in range(len(arr) - 1):
-            if arr[i] > arr[i+1]:
-                arr[i],arr[i+1] = arr[i+1],arr[i]
-                sorted = False
-    return arr
+#take a 64-bit word and return the 64-bit word consisting of the bits of the input word in reverse order
 
+def reverse_bits(x: int) -> int:
+    rev = 0
+    
+    while x > 0:
+        rev = rev << 1
+        if (x & 1 == 1):
+            rev = rev ^ 1
+
+        x = x >> 1
+    
+    return rev
  
