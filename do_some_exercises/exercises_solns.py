07c1eca3f5eb31716bedabdf3639eefaae08e3b1
Leland Krych <Leland.Krych@gmail.com>
2019-12-16 07:30:51 -0800

    get some exercise

--- do_some_exercises/exercises_solns.py
+++ do_some_exercises/exercises_solns.py
@@ -1,12 +1,21 @@
-from typing import List
+# the trick to this problem is to think about the gradeschool multiplication algorithm
+# this algorithm uses left shifts for numbers beyond the least significant digit
 
-def reverse( s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        def helper(left, right):
-            if left < right:
-                s[left], s[right] = s[right], s[left]
-                helper(left + 1, right - 1)
-        helper(0, len(s) - 1)
+def multiply(x: int, y: int) -> int:
+    #helper function
+    def add(a: int, b: int) -> int:
+        running_sum, carryin, k, temp_a, temp_b = 0, 0, 1, a, b
+        while temp_a or temp_b:
+            ak, bk = a & k, b & k
+            carryout = (ak & bk) | (ak & carryin) | (bk & carryin)
+            running_sum |= ak ^ bk ^ carryin
+            carryin, k, temp_a, temp_b = (carryout << 1, k << 1, temp_a >> 1, temp_b >> 1)
+        return running_sum | carryin
+    
+    running_sum = 0
+    while x: #examine each bit of x
+        if x & 1:
+            running_sum = add(running_sum, y)
+        x, y = x >> 1, y << 1
+    return running_sum
  
